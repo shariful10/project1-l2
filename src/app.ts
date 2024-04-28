e42e48f2d111c2a6a6552b346e8a68e4310dee3b
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-04-29 00:45:37 +0600

    post

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,18 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
+// parsers
+app.use(express.json())
+
 app.get("/", (req: Request, res: Response) => {
-	res.send("Hello from World!");
+	res.send("Hello from Worlddd!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+	console.log(req.body)
+  res.json({
+    message: "Successfully recivied data"
+  })
 });
 
 export default app;
