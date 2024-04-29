abfdf68c8ecf7d5e4b9469e3dcb97871b748551d
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-04-29 17:21:36 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,41 @@ app.use(express.json());
 // routers
 
 const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+	const user = req.body;
+	console.log(user);
+
+	res.json({
+		success: true,
+		message: "User is created successfully",
+		data: user,
+	});
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+	const course = req.body;
+	console.log(course);
+
+	res.json({
+		success: true,
+		message: "Course is created successfully",
+		data: course,
+	});
+});
+
+// global error handler
+
+app.all("*", (req: Request, res: Response) => {
+	res.status(400).json({
+		success: false,
+		message: "Not found",
+	});
+});
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
 	console.log(req.url, req.method, req.hostname);
